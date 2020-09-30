b321d8aa4960736ddc484151b8b609db3ddd366e
고규훈 <kh.ko@airpremia.com>
2020-09-30 14:07:19 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'papago-subtitle';
+  original = '';
+  result = '';
+
+  constructor() {
+    const cache = localStorage.getItem('resource');
+    if (cache) {
+      this.original = cache;
+      this.onClick();
+    }
+  }
+
+  compile(): void {
+    const result = this.original.split('\n\n').map((p: string) => {
+      return p.split('\n');
+    });
+    this.result = result;
+  }
+
+  onClick(): void {
+    this.compile();
+    localStorage.setItem('resource', this.original);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
